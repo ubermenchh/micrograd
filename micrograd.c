8160875176fecc7de26f5fe52519668f64e46b30
ubermenchh <umangkaushik46.bs@gmail.com>
2024-07-24 01:26:42 +0530

    added modules

--- micrograd.c
+++ micrograd.c
@@ -234,12 +234,255 @@ void log_backward(Context* ctx, float grad) {
     in->grad += (1 / in->data) * grad;
 }
 
+float random_uniform() {
+    return 2.f * ((float)rand() / RAND_MAX) - 1.f;
+}
+
+Neuron* init_neuron(int nin, bool nonlin) {
+    Neuron* neuron = (Neuron*)malloc(sizeof(Neuron));
+    if (neuron == NULL) return NULL;
+    neuron->base.impl = neuron;
+    neuron->base.parameters = neuron_parameters;
+    neuron->base.free = free_neuron;
+
+    neuron->nin = nin;
+    neuron->nonlin = nonlin;
+    neuron->forward = neuron_forward;
+
+    neuron->w = (Value**)malloc(nin * sizeof(Value));
+    if (neuron->w == NULL) {
+        free(neuron);
+        return NULL;
+    } 
+
+    float scale = sqrtf((float)nin);
+    for (int i=0; i<nin; i++) {
+        neuron->w[i] = init_value(random_uniform() * powf(nin, -0.5f));
+        if (neuron->w[i] == NULL) {
+            for (int j = 0; j < i; j++) 
+                free(neuron->w[j]);
+            free(neuron->w);
+            free(neuron);
+            return NULL;
+        }
+    }
+
+    neuron->b = init_value(0.0);
+    if (neuron->b == NULL) {
+        for (int i=0; i < nin; i++) 
+            free(neuron->w[i]);
+        free(neuron->w);
+        free(neuron);
+        return NULL;
+    }
+
+    return neuron;
+}
+
+void free_neuron(Module* module) {
+    Neuron* neuron = (Neuron*)module->impl;
+    for (int i = 0; i < neuron->nin; i++) {
+        free_value(neuron->w[i]);
+    }
+    free(neuron->w);
+    free_value(neuron->b);
+    free(neuron);
+}
+
+Value* neuron_forward(Neuron* neuron, Value** x) {
+    Value* out = init_value(0.0);
+    for (int i = 0; i < neuron->nin; i++) {
+        out->data += mul_value(neuron->w[i], x[i])->data;
+        out = add_value(out, neuron->b);
+    }
+    if (neuron->nonlin)
+        tanh_value(out);
+    return out;
+}
+
+Value** neuron_parameters(Module* module) {
+    Neuron* neuron = (Neuron*)module->impl;
+    Value** params = malloc((neuron->nin + 1) * sizeof(Value)); // +1 for bias
+    
+    for (int i = 0; i < neuron->nin; i++) {
+        params[i] = neuron->w[i];
+    }
+    params[neuron->nin] = neuron->b;
+    return params;
+}
+
+Layer* init_layer(int nin, int nout, bool nonlin) {
+    Layer* layer = (Layer*)malloc(sizeof(Layer));
+    if (layer == NULL) return NULL;
+    layer->base.impl = layer;
+    layer->base.parameters = layer_parameters;
+    layer->base.free = free_layer;
+    
+    layer->nin = nin;
+    layer->nout = nout;
+    layer->forward = layer_forward;
+
+    layer->neurons = (Neuron**)malloc(nout * sizeof(Neuron*));
+    if (layer->neurons == NULL) {
+        free(layer);
+        return NULL;
+    }
+
+    for (int i = 0; i < nout; i++) {
+        layer->neurons[i] = init_neuron(nin, nonlin);
+        if (layer->neurons[i] == NULL) {
+            for (int j = 0; j < layer->nout; j++)
+                free_neuron((Module*)layer->neurons[j]);
+            free(layer->neurons);
+            free(layer);
+            return NULL;
+        }
+    }
+    return layer;
+}
+
+void free_layer(Module* module) {
+    Layer* layer = (Layer*)module->impl;
+    for (int i = 0; i < layer->nin; i++) {
+        free_neuron((Module*)layer->neurons[i]);
+    }
+    free(layer->neurons);
+    free(layer);
+}
+
+Value** layer_forward(Layer* layer, Value** input) {
+    Value** out = (Value**)malloc(layer->nout * sizeof(Value*));
+    if (out == NULL) return NULL;
+
+    for (int i = 0; i < layer->nout; i++) {
+        out[i] = layer->neurons[i]->forward(layer->neurons[i], input);
+        if (out[i] == NULL) {
+            for (int j = 0; j < i; j++) {
+                free_value(out[j]);
+            }
+            free(out);
+            return NULL;
+        }
+    }
+    return out;
+}
+
+Value** layer_parameters(Module* module) {
+    Layer* layer = (Layer*)module->impl;
+    int total_params = 0;
+
+    for (int i = 0; i < layer->nout; i++) {
+        total_params += layer->nin + 1;
+    }
+
+    Value** params = (Value**)malloc(total_params * sizeof(Value*));
+    if (params == NULL) return NULL;
+
+    int param_idx = 0;
+    for (int i = 0; i < layer->nout; i++) {
+        Value** neuron_params = layer->neurons[i]->base.parameters((Module*)layer->neurons[i]);
+        for (int j = 0; j < layer->nin + 1; j++) {
+            params[param_idx++] = neuron_params[j];
+        }
+        free(neuron_params);
+    }
+    return params;
+}
+
+MLP* init_mlp(int nin, int nouts, bool nonlin) {
+    MLP* mlp = (MLP*)malloc(sizeof(MLP));
+    if (mlp == NULL) return NULL;
+    
+    mlp->base.impl = mlp;
+    mlp->base.parameters = mlp_parameters;
+    mlp->base.free = free_mlp;
+
+    mlp->forward = mlp_forward;
+    mlp->nin = nin;
+    mlp->nouts = nouts;
+    
+    mlp->layers = (Layer**)malloc(nouts * sizeof(Layer*));
+    if (mlp->layers == NULL) {
+        free(mlp);
+        return NULL;
+    }
+    
+    for (int i = 0; i < nouts; i++) {
+        mlp->layers[i] = init_layer(nin, nouts, nonlin);
+        if (mlp->layers[i] == NULL) {
+            for (int j = 0; j < i; j++) {
+                free_layer((Module*)mlp->layers[j]);
+            }
+            free(mlp->layers);
+            free(mlp);
+            return NULL;
+        }
+    }
+    return mlp;
+}
+
+void free_mlp(Module* module) {
+    MLP* mlp = (MLP*)module->impl;
+    for (int i = 0; i < mlp->nouts; i++) {
+        free_layer((Module*)mlp->layers[i]);
+    }
+    free(mlp->layers);
+    free(mlp);
+}
+
+Value** mlp_forward(MLP* mlp, Value** input) {
+    Value** out = input;
+    Value** prev_out = NULL;
+    if (out == NULL) return NULL;
+
+    for (int i = 0; i < mlp->nouts; i++) {
+        prev_out = out;
+        out = mlp->layers[i]->forward(mlp->layers[i], out);
+        if (out == NULL) 
+            return NULL;
+
+        if (i > 0) {
+            for (int j = 0; j < mlp->layers[i-1]->nout; j++) {
+                free_value(prev_out[j]);
+            }
+            free(prev_out);
+        }
+    }
+    return out;
+}
+
+Value** mlp_parameters(Module* module) {
+    MLP* mlp = (MLP*)module->impl;
+    int total_params = 0;
+
+    for (int i = 0; i < mlp->nouts; i++) {
+        Value** layer_params = mlp->layers[i]->base.parameters((Module*)mlp->layers[i]);
+        int layer_param_count = mlp->layers[i]->nin * mlp->layers[i]->nout + mlp->layers[i]->nout;
+        total_params += layer_param_count;
+        free(layer_params);
+    }
+
+    Value** params = (Value**)malloc(total_params * sizeof(Value*));
+    if (params == NULL) return NULL;
+
+    int param_index = 0;
+    for (int i = 0; i < mlp->nouts; i++) {
+        Value** layer_params = mlp->layers[i]->base.parameters((Module*)mlp->layers[i]);
+        int layer_param_count = mlp->layers[i]->nin * mlp->layers[i]->nout + mlp->layers[i]->nout;
+        for (int j = 0; j < layer_param_count; j++) {
+            params[param_index++] = layer_params[j];
+        }
+        free(layer_params);
+    }
+    return params;
+}
+
 int main(void) {
     Value* a = init_value(10.0);
     Value* b = init_value(3.0);
+    Neuron* n = init_neuron(1, false);
 
-    Value* out = log_value(a);
-    out = mul_value(out, b);
+    Value* out = neuron_forward(n, &a);
     backward(out);
     print_value(out);
     print_value(a); print_value(b);
--- micrograd.h
+++ micrograd.h
@@ -53,3 +53,59 @@ void exp_backward(Context* ctx, float grad);
 
 Value* log_value(Value* in);
 void log_backward(Context* ctx, float grad);
+
+// Layers 
+typedef struct Module Module;
+typedef struct Neuron Neuron;
+typedef struct Layer Layer;
+typedef struct MLP MLP;
+
+struct Module {
+    void* impl;
+    Value** (*parameters)(Module* self);
+    void (*free)(Module* self);
+};
+
+struct Neuron {
+    Module base;
+    int nin;
+    bool nonlin;
+
+    Value** w;
+    Value* b;
+    Value* (*forward)(Neuron* self, Value** input);
+};
+
+struct Layer {
+    Module base;
+    int nin;  // input dim
+    int nout; // output dim
+
+    Neuron** neurons;
+    Value** (*forward)(Layer* self, Value** input);
+}; 
+
+struct MLP {
+    Module base;
+    int nin;
+    int nouts;
+
+    Layer** layers; 
+    Value** (*forward)(MLP* self, Value** input);
+};
+
+Neuron* init_neuron(int nin, bool nonlin);
+Value* neuron_forward(Neuron* Neuron, Value** x);
+Value** neuron_parameters(Module* module);
+void free_neuron(Module* module);
+
+Layer* init_layer(int nin, int nouts, bool nonlin);
+Value** layer_forward(Layer* layer, Value** x);
+Value** layer_parameters(Module* module);
+void free_layer(Module* module);
+
+MLP* init_mlp(int nin, int nouts, bool nonlin);
+Value** mlp_forward(MLP* mlp, Value** x);
+Value** mlp_parameters(Module* module);
+void free_mlp(Module* module);
+
